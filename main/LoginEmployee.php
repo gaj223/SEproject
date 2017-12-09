6ee7fb1fac6d1ac12052c1a280bf4b74a61fe6e4
root <root@emis-vm.c.emis-project.internal>
2017-12-09 06:53:19 +0000

    Fixed some bugs

--- main/LoginEmployee.php
+++ main/LoginEmployee.php
@@ -46,71 +46,74 @@ Validates the employee and create the Welcome page for the employee
             
             //set the default client character set
             mysqli_set_charset($con, 'utf-8');
-
-            // Check to see if username and password is correct
-            $user = htmlentities($_POST["user2"]);
-            $passwrd =  htmlentities($_POST["passwrd2"]);
+	    if (! session_id()) session_start();
+	    else if (! isset($_SESSION['login'])) {
+		$_SESSION['login'] = 1;
+	
+            	// Check to see if username and password is correct
+            	$user = htmlentities($_POST["user2"]);
+            	$passwrd =  htmlentities($_POST["passwrd2"]);
             
-            // check if user is in the database
-            $employeeUsr = mysqli_query($con, "SELECT FirstName FROM EmployeeLogin WHERE Email='" . $user . "'");
-            if (mysqli_num_rows($employeeUsr) < 1) {
-                echo "<h1 style='font-size:70px;text-align:left;'> "
-                . "<img src='Symbol.png' width='60' height='60'>"
-                        . "EMIS <hr> </h1>";
-                echo "<p>The Username and/or Password entered was incorrect. Please check and try again. <br>"
-                        . "<a href='Username.php'> Forgot Username?</a><br>"
-                        . "<a href='Password.php'> Forgot Password? </a><br></p>";
-                echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
-            }
-            $row = mysqli_fetch_row($employeeUsr);
-            $firstNm = $row[0];
+            	// check if user is in the database
+            	$employeeUsr = mysqli_query($con, "SELECT FirstName FROM EmployeeLogin WHERE Email='" . $user . "'");
+            	if (mysqli_num_rows($employeeUsr) < 1) {
+                	echo "<h1 style='font-size:70px;text-align:left;'> "
+                	. "<img src='Symbol.png' width='60' height='60'>"
+                        	. "EMIS <hr> </h1>";
+                	echo "<p>The Username and/or Password entered was incorrect. Please check and try again. <br>"
+                        	. "<a href='Username.php'> Forgot Username?</a><br>"
+                        	. "<a href='Password.php'> Forgot Password? </a><br></p>";
+                	echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
+            	}
+            	$row = mysqli_fetch_row($employeeUsr);
+            	$firstNm = $row[0];
             
-            $pas = mysqli_query($con, "SELECT EmployeeID FROM EmployeeLogin WHERE Email='" . $user . "'");
-            $pass = $pas->fetch_assoc();
+            	$pas = mysqli_query($con, "SELECT EmployeeID FROM EmployeeLogin WHERE Email='" . $user . "'");
+            	$pass = $pas->fetch_assoc();
             
-            // check if account is locked 
-            $lock = mysqli_query($con, "SELECT Locked FROM EmployeeLogin WHERE Email='" . $user . "'");
-            $row = mysqli_fetch_row($lock);
-            if(strcmp(row[0],"Yes") == 0){
-                echo "<h1 style='font-size:70px;text-align:left;'> "
-                . "<img src='Symbol.png' width='60' height='60'>"
-                        . "EMIS <hr> </h1>";
-                echo "<p> Your account has been locked. Please, contact the clinic or click the link to recovery account.<br>"
-                            . "<a href='Password.php'> Account Recovery  </a><br></p>";
-                echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
-                exit();
-            }
+            	// check if account is locked 
+            	$lock = mysqli_query($con, "SELECT Locked FROM EmployeeLogin WHERE Email='" . $user . "'");
+            	$row = mysqli_fetch_row($lock);
+            	if(strcmp(row[0],"Yes") == 0){
+                	echo "<h1 style='font-size:70px;text-align:left;'> "
+                	. "<img src='Symbol.png' width='60' height='60'>"
+                        	. "EMIS <hr> </h1>";
+                	echo "<p> Your account has been locked. Please, contact the clinic or click the link to recovery account.<br>"
+                        	    . "<a href='Password.php'> Account Recovery  </a><br></p>";
+                	echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
+                	exit();
+            	}
             
-            // Check if password is correct
-            $employeePW = mysqli_query($con, "SELECT Password FROM EmployeeLogin WHERE Email='" . $user . "'");
-            $row = mysqli_fetch_row($employeePW);
-            if ($passwrd != $row[0]) {
-                // Track attempts
-                echo "<h1 style='font-size:70px;text-align:left;'> "
-                . "<img src='Symbol.png' width='60' height='60'>"
-                        . "EMIS <hr> </h1>";
-                $attempt = mysqli_query($con, "SELECT attempts FROM EmployeeLogin WHERE Email='" . $user . "'");
-                $row = mysqli_fetch_row($attempt);
-                $count = $row[0] + 1;
-                mysqli_query($con, "UPDATE EmployeeLogin SET attempts='" . $count . "' WHERE Email='" . $user . "'");
-                if($count >= 5){ // lockout account
-                    mysqli_query($con, "UPDATE EmployeeLogin SET Locked='Yes' WHERE Email='" . $user . "'");
-                    // Print out that account is locked
-                    echo "<p> Your account has been locked. Please, contact the clinic or click the link to recovery account.<br>"
-                            . "<a href='Password.php'> Account Recovery  </a><br></p>";
-                    echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
+            	// Check if password is correct
+            	$employeePW = mysqli_query($con, "SELECT Password FROM EmployeeLogin WHERE Email='" . $user . "'");
+            	$row = mysqli_fetch_row($employeePW);
+            	if ($passwrd != $row[0]) {
+                	// Track attempts
+                	echo "<h1 style='font-size:70px;text-align:left;'> "
+                	. "<img src='Symbol.png' width='60' height='60'>"
+                        	. "EMIS <hr> </h1>";
+                	$attempt = mysqli_query($con, "SELECT attempts FROM EmployeeLogin WHERE Email='" . $user . "'");
+                	$row = mysqli_fetch_row($attempt);
+                	$count = $row[0] + 1;
+                	mysqli_query($con, "UPDATE EmployeeLogin SET attempts='" . $count . "' WHERE Email='" . $user . "'");
+                	if($count >= 5){ // lockout account
+                    		mysqli_query($con, "UPDATE EmployeeLogin SET Locked='Yes' WHERE Email='" . $user . "'");
+                    		// Print out that account is locked
+                    		echo "<p> Your account has been locked. Please, contact the clinic or click the link to recovery account.<br>"
+                            	. "<a href='Password.php'> Account Recovery  </a><br></p>";
+                    		echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
                     
-                }else{
-                    // Print out link back to home page and username and password recovery
-                    echo "<p>The Username and/or Password entered was incorrect. Please check and try again. <br>"
-                            . "<a href='Username.php'> Forgot Username?</a><br>"
-                            . "<a href='Password.php'> Forgot Password? </a><br></p>";
-                    echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
-                }
-                exit();
+                	}else{
+                    		// Print out link back to home page and username and password recovery
+                    		echo "<p>The Username and/or Password entered was incorrect. Please check and try again. <br>"
+                            		. "<a href='Username.php'> Forgot Username?</a><br>"
+                            		. "<a href='Password.php'> Forgot Password? </a><br></p>";
+                    			echo "<p style = text-align:right;> <br><a href='index.php'> Home </a><br>";
+                	}
+                	exit();
+            	}
+            	mysqli_query($con, "UPDATE EmployeeLogin SET attempts= '0' WHERE Email='" . $user . "'");
             }
-            mysqli_query($con, "UPDATE EmployeeLogin SET attempts= '0' WHERE Email='" . $user . "'");
-            
             echo 'Welcome to EMIS, '. $firstNm ."<br/>";
             
             // Free mysql query
@@ -126,9 +129,9 @@ Validates the employee and create the Welcome page for the employee
         </h1>
         <p> 
             Menu: <br>
-            <a href="search.php?employeeid=<?php echo $pass['EmployeeID']; ?>"> Search</a> <br>
+            <a href="../jamesfiles/search.php?employeeid=<?php echo $pass['EmployeeID']; ?>"> Search</a> <br>
             <a href="../createapp/create.php"> Create Appointment</a> <br>
-            <a href="CreatePatientAccount.php"> Create a New Patient Account</a> <br>
+            <a href="../createpatient/CreatePatientAccount.php"> Create a New Patient Account</a> <br>
         </p>
         
     </body>
