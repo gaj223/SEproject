0b65cfcaa8027cc66d1d2a1a1e5a21588ad6d005
root <root@emis-vm.c.emis-project.internal>
2017-12-14 01:37:08 +0000

    small change

--- install/install.php
+++ install/install.php
@@ -5,6 +5,7 @@ Please read README.txt before installation.<br>
 MySQL Username: <input type="text" name="user" id="user"><br>
 Password:	<input type="text" name="password" id="password"><br>
 Host:		<input type="text" name="host" id="host"><br>
+<input type="submit" value="submit">
 </form>
 </body>
 </html>
